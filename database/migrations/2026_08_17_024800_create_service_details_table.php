e286ba3172835ebacda0a266746a61f1df71384f
infomotin <motin.mmk.jr@gmail.com>
2026-08-17 08:49:35 +0600

    chore: initialize new Laravel project
    add full Laravel project skeleton including default configs, routes, and custom database migrations

---
+++ database/migrations/2026_08_17_024800_create_service_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->string('image');
+            $table->string('title');
+            $table->text('description');
+            $table->string('icon')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_details');
+    }
+};
